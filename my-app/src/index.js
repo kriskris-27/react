8e6a4e44c4e9c963f4eefe50ae0b666f3b6c8a02
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-21 11:23:30 +0530

    parent and children component example

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function IlikeinReact() 
+function Header()
 {
   return(
-    <>
-    <header>
+<header>
       <nav>
         <img src="image.png" width="50px"/>
       </nav>
     </header>
+  )
+}
+
+function Footer()
+{
+  return(
+    <foorter>
+      <small>© 2024 kris development. All rights reserved.</small>
+    
+    </foorter>
+  )
+}
+
+function Body()
+{
+  return(
+    <>
     <h1>I like React because</h1>
     <ol>
       <li>i like components</li>
       <li>i like imperative</li>
       <li>I like reusability</li>
     </ol>
-    <foorter>
-      <small>© 2024 kris development. All rights reserved.</small>
-    </foorter>
+    </>
+  )
+}
+function IlikeinReact() 
+{
+  return(
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
     </>
   )
 }
