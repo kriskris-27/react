53208d93d6c4d16ee01d1bfb9c95054c6b78cf23
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-21 11:01:04 +0530

    from rendering to first component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const page = (
-  <div>
+
+
+function TemporaryName() {
+  return (
+    <div>
     <img src="image.png" width="40px"/>
     <h1>Fun facts about React</h1>
     <ul>
       <li>Was first releasead in 2013</li>
       <li>Was first releasead in 2013</li>  
       <li>Was first releasead in 2013</li>
-      <li>Is maintaied by FaceBook</li>
+      <li>Is maintaied by FaceBook
+
+      </li>
     </ul>
   </div>
-);
+  )
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(page);
+ReactDOM.createRoot(document.getElementById("root")).render(<TemporaryName/>);
