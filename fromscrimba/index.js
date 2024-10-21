6ed2181b530f1571a6d3d4630d4be4e5f520153d
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-21 10:06:22 +0530

    basic react code executable commit

---
+++ fromscrimba/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Reactdom from "react-dom"
+
+const page = (
+    <h1>Tessting</h1>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(page);
---
+++ my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const page = <h1>Testing</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(page);
